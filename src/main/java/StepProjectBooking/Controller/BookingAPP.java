cab42dfe0db6de8c9d39fbec2cbd2b1f927fbf95
elgun.cumayev <elgun.cumayev@mail.ru>
2020-03-16 15:37:47 +0400

    StepProjectController

--- src/main/java/StepProjectBooking/Controller/BookingAPP.java
+++ src/main/java/StepProjectBooking/Controller/BookingAPP.java
@@ -1,4 +1,35 @@
 package StepProjectBooking.Controller;
 
 public class BookingAPP {
+
+  public void run() {
+    Controller controller = new Controller();
+    Console cc = new Console();
+    while (true) {
+      cc.println(controller.showMenu());
+      String menuChoice = cc.readline();
+      switch (menuChoice) {
+        case "1":
+          controller.case1OP();
+          break;
+        case "2":
+          controller.case2OP();
+          break;
+        case "3":
+          controller.case3OP();
+          break;
+        case "4":
+          controller.case4OP();
+          break;
+        case "5":
+          controller.case5OP();
+          break;
+        case "6":
+          cc.println("Good bye!");
+          return;
+        default:
+          cc.println("Wrong Input. Please try again.");
+      }
+    }
+  }
 }
